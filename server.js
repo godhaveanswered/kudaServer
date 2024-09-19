12c655269e67547deb2fca931c47d6f95d73f6fc
zubikhush6 <165460773+zubikhush6@users.noreply.github.com>
2024-09-19 23:08:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "frankline3576@gmail.com";
 const pass = "mvklwycynflkvuhw";
-// 10 nov
+// emeka p you 10 oct
 
 // Middleware
 app.use(express.json());

2ac53533ca98635726872d3d176a25a3f7fcbf00
godhaveanswered <godhaveanswered@gmail.com>
2025-03-03 06:16:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "godhaveanswered@gmail.com";
-const pass = "vlnhddgacwdcrncx";
+//const pass = "vlnhddgacwdcrncx";
 // mmagha 3 march
 
 // Middleware
